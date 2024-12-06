7dfcd4f82e3cf16166518d0de91d918452986bb7
Konst <brskkonstantin@gmail.com>
2024-12-06 20:35:20 +0700

    добавление тестов из пункта 2.1

---
+++ tests.cpp
@@ -0,0 +1,30 @@
+#include "tests.h"
+
+bool test1_body_contains_boundary_values()
+{
+	Price open = 10;
+	Price high = 50;
+	Price low = 0;
+	Price close = 30;
+	Candle candle(open, high, low, close);
+	return candle.body_contains(10) && candle.body_contains(30);
+}
+bool test1_body_contains_external_values()
+{
+	Price open = 10;
+	Price high = 50;
+	Price low = 0;
+	Price close = 30;
+	Candle candle(open, high, low, close);
+	return !candle.body_contains(9.9) && !candle.body_contains(30.1);
+}
+bool test1_body_contains_internal_values()
+{
+	Price open = 10;
+	Price high = 50;
+	Price low = 0;
+	Price close = 30;
+	Candle candle(open, high, low, close);
+	return candle.body_contains(10.1) && candle.body_contains(29.9);
+}
+
--- tests.h
+++ tests.h
@@ -1,1 +1,6 @@
 #pragma once
+#include "candle.h"
+
+bool test1_body_contains_boundary_values();
+bool test1_body_contains_external_values();
+bool test1_body_contains_internal_values();
